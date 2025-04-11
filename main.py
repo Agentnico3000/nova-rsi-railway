3e1c34597b53fab5fa5373321a94292fe091786a
Agentnico3000 <dax.williams@gmail.com>
2025-04-11 18:13:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -43,5 +43,5 @@ def health():
     return jsonify({'status': 'online'})
 
 if __name__ == "__main__":
-    port = int(os.getenv('PORT', 5000))
+    port = int(os.getenv('PORT', 8080))
     app.run(host="0.0.0.0", port=port)
